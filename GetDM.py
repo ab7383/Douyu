6064f869e778677bc7a58d108b48d48b98bc6dfc
378374573@qq.com <root@LinuxForPthon3.(none)>
2016-10-14 17:32:21 +0800

    10.14

--- GetDM.py
+++ GetDM.py
@@ -10,32 +10,31 @@ from danmu import *
 
 
 def  main():
-  i = 1
-  count_Heart = 0
-  conut = 0
   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-  s.setsockopt(socket.SOL_SOCKET,socket.SO_KEEPALIVE,1)
+#  s.setsockopt(socket.SOL_SOCKET,socket.SO_KEEPALIVE,1)
   rid = input('please enter a room id:')
   GetDm = GetDanmu(rid)
+  thread_list = []
 
-
-  GetDm.connect(s) 
-  GetDm.login(s)
   if GetDm.Is_alive():
-    while i:
+    t2 = threading.Thread(target = GetDm.Heart, args=(s,))
+    t1 = threading.Thread(target = run, args=(s,GetDm,))
+    t1.start()
+    t2.start()
+  else:
+    GetDm.Logout(s)
+    print('the live_room has closed')
+
+
+def run(s,GetDm):
+    GetDm.connect(s)
+    GetDm.login(s)
+
+    while 1:
       byt = s.recv(1024)
       DM = GetDm.clean(byt) 
-      if count_Heart > 5:
-        if GetDm.Is_alive():
-           count_Heart = 0
-           GetDm.Heart(s)
-        else:
-           break
       if DM == None:
-        count_Heart += 1
         continue
       print(DM)
       print('\n')
-  else:
-    print('the live_room has closed')
 main()
