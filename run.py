3df4969caf2071f9da2ff2c3a1c5a0c759f8c032
ab7383 <378374573@qq.com>
2016-12-04 02:24:01 -0800

    2016.12.4

--- run.py
+++ run.py
@@ -4,25 +4,37 @@ from danmu import *
 import sys
 import multiprocessing
 
-rid = ['156277','78561']
-limit_time = input ('how long would you want to Get: ')
 
-Get1 = GetDanmu(rid[0],limit_time)
-Get2 = GetDanmu(rid[1],limit_time)
-Get = [Get1,Get2]
-def GetDM(Get):
-  Get.run()
-#多进程写法
-#p_1 = multiprocessing.Process(target = GetDM, args=(Get1,))
-#p_2 = multiprocessing.Process(target = GetDM, args=(Get2,))
-#p_1.start()
-#p_2.start()
-#p_1.join()
-#p_2.join()
-#进程池写法
-pool = multiprocessing.Pool(processes = 2)
-for i in range(len(rid)):
-  pool.apply_async(GetDM,(Get[i],))
+limit_time = 3600
+def read_room(limit = 6):
+  Ancho_list = []
+  with open("./room_list","r") as f:
+    Anchos = f.readlines()
+    for line in Anchos:
+      Ancho = line.split()
+      Ancho_list.append(Ancho)
+    return Ancho_list[:limit]
+
+def Get(dy):
+  dy.run()
+
+Anchos = read_room()
+
+#创建GetDanmu对象
+dy = []
+j = 0
+for room in Anchos:
+  room = GetDanmu(Anchos[j][1],Anchos[j][0],limit_time)
+  dy.append(room)
+  j =j + 1
+#进程池
+pool = multiprocessing.Pool(processes = 10)
+for i in range(len(Anchos)):
+  pool.apply_async(Get,(dy[i],))
 pool.close()
 pool.join()
+
+
+
+
 
