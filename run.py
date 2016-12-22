31e2ad788d4cc82e7bd4d01bba122d49c0fde34c
ab7383 <378374573@qq.com>
2016-12-22 00:12:31 -0800

    12.22

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import sys
 import multiprocessing
 
 
-limit_time = 36000
-def read_room(limit = 6):
+limit_time = 7200
+def read_room(limit = 5):
   Ancho_list = []
   with open("./room_list","r") as f:
     Anchos = f.readlines()
@@ -25,6 +25,7 @@ dy = []
 j = 0
 for room in Anchos:
   room = GetDanmu(Anchos[j][1],Anchos[j][0],limit_time)
+  #room = GetDanmu('yinzi',71415,limit_time)
   dy.append(room)
   j =j + 1
 #进程池
